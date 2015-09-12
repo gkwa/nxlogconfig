fe07549579ef7e246d4aee8784ab6a2ef37e8f41
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-12 03:02:00 -0700

    yada-yada

--- nxlog.bat
+++ nxlog.bat
@@ -3,7 +3,6 @@ cd "C:\Program Files (x86)\nxlog"
 rem create timestamp to rename old nxlog.log
 for /F "Tokens=1-7 Delims=/:. " %%d In ("%Date%%Time%") Do Set Now=%%e%%f%%g%%h%%i%%j
 
-FOR %%A IN (%Date:/=%) DO SET Today=%%A
 taskkill /F /IM nxlog.exe 2>NUL
 move data\nxlog.log data\nxlog-%now%.log
 rem del data\configcache.dat
