511cb16d721c53131a3baeeac17fc9cd2f044557
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-12 00:37:03 -0700

    Working with good iteration

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
-t2:
-	cygstart --hide nxlog.bat
+t2: data/access.2015.09.07.log
+	cygstart --minimize nxlog.bat
 	sleep 2
 	du -sh data/test.log
 	tail -10 data/nxlog.log
+	-find . -iname configcache.dat
+	-find . -iname '*.dat'
+
+data/access.2015.09.07.log:
+	7z x -odata access.2015.09.07.log.7z
+
+size:
+	du -sh data/test.log
--- nxlog.bat
+++ nxlog.bat
@@ -1,6 +1,11 @@
 cd "C:\Program Files (x86)\nxlog"
 
+rem create timestamp to rename old nxlog.log
+for /F "Tokens=1-7 Delims=/:. " %%d In ("%Date%%Time%") Do Set Now=%%e%%f%%g%%h%%i%%j
+
+FOR %%A IN (%Date:/=%) DO SET Today=%%A
 taskkill /F /IM nxlog.exe 2>NUL
-del data\configcache.dat
+move data\nxlog.log data\nxlog-%now%.log
+rem del data\configcache.dat
 del data\test.log
 nxlog.exe -f nxlog.conf
